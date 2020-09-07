5eb8154ef9647e07d6e33281e4be51faf581f138
taka-nagamune <69788181+taka-nagamune@users.noreply.github.com>
2020-09-07 17:10:52 +0900

    レビューに対しての修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nick_name, presence: true
+  
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
-    validates :first_name
-    validates :last_name
-  end
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カナを使用してください'} do
-    validates :first_name_kana, presence: true
-    validates :last_name_kana, presence: true
-  end
+
+  NAME_REGEX =  /\A[ぁ-んァ-ン一-龥]+\z/
+  validates_format_of :first_name, :last_name, with: NAME_REGEX, message: 'には全角文字を使用してください'
+  
+
+  NAME_KANA_REGIX = /\A[ァ-ン]+\z/
+  validates_format_of :first_name_kana, :last_name_kana, with: NAME_KANA_REGIX, message: 'には全角カナを使用してください'
+  
   validates :birthday, presence: true
 end
