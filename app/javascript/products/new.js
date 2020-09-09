48d1272dd39c735662ab0a3bf38a0ec5cdf06b96
taka-nagamune <69788181+taka-nagamune@users.noreply.github.com>
2020-09-09 15:03:26 +0900

    JSでの自動手数料計算の実装

---
+++ app/javascript/products/new.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function(){
+  const totalPrice = document.getElementById("item-price");
+  totalPrice.addEventListener('input',function(){
+    const inputPrice = document.getElementById("item-price").value;
+    const salesCharge = inputPrice / 10;
+    const profit = inputPrice - salesCharge;
+    const textSalesCharge = document.getElementById("add-tax-price").textContent = salesCharge
+    const textProfit = document.getElementById("profit").textContent = profit
+    // console.log(profit)
+  })
+})
