76960c032c603edf2b61ac382b34facc1ed31334
taka-nagamune <69788181+taka-nagamune@users.noreply.github.com>
2020-09-09 15:04:10 +0900

    ログイン画面への遷移

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,33 @@
 class ProductsController < ApplicationController
-  before_action :move_to_index, except: [:index]
+  before_action :move_to_sign_in, except: [:index]
+  # before_action :authenticate_user!, except: [:index]
   def index
     @products = Product.all
   end
 
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
-  end
 
   def new
     @product = Product.new
   end
+  
 
   def create
-    # Product.create(product_params)
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
 
   private
   def product_params
-    params.require(:product).permit(:name, :content, :price)
+    params.require(:product).permit(:name, :content, :price, :image, :shipping_charge_id, :shipping_region_id, :shipping_date_id, :category_id, :condition_id).merge(user_id: current_user.id)
   end
+
+  def move_to_sign_in
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
 end
