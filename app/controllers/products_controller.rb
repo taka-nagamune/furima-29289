19394593b6be6b59c6939c01cfadd810370d0dc1
taka-nagamune <69788181+taka-nagamune@users.noreply.github.com>
2020-09-11 17:17:40 +0900

    レビュー修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,7 +33,7 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    if     @product.destroy
+    if @product.destroy
       redirect_to root_path
     else
       render:destroy
