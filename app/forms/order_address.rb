32ca70c7afc4fb2a2cff0c5d4ce939456813c986
taka-nagamune <69788181+taka-nagamune@users.noreply.github.com>
2020-09-18 14:45:04 +0900

    正規表現を変数に代入

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -3,14 +3,19 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :token, :postal_code, :shipping_region_id, :city, :address, :building_name, :call_number,:product_id, :user_id
 
+  POSTAL_CODE_REGEX = /\A\d{3}[-]\d{4}\z/
+  HARF_SIZE_NUMBER = /\A[0-9]+\z/
+
   with_options presence: true do
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
+    validates :postal_code,format: {with: POSTAL_CODE_REGEX}
     validates :city
     validates :address
-    validates :call_number, format: {with: /\A[0-9]+\z/}, length: {maximum: 11} 
+    validates :call_number, format: {with: HARF_SIZE_NUMBER}, length: {maximum: 11} 
     validates :token
     validates :shipping_region_id, numericality: {other_than: 1}
   end
+
+
 
 
   def save
