4dc498c8a1061ecf4e821ca63617b64ed91823cd
taka-nagamune <69788181+taka-nagamune@users.noreply.github.com>
2020-10-05 16:15:45 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
   end
+  
 end
